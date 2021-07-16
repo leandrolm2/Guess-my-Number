a6882f1fe708da050753f6605e8a31b155391ea3
Leandro Melgarejo <53884524+leandrolm2@users.noreply.github.com>
2021-07-16 13:32:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 'use strict';
-/*
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct number!';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-*/
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
 
@@ -49,28 +40,6 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
-
-  // else if (guess > secretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector('.message').textContent = 'Too high';
-  //     score--;
-  //     document.querySelector('.score').textContent = score;
-  //   } else {
-  //     document.querySelector('.message').textContent = 'You lose the game';
-  //     document.querySelector('.score').textContent = 0;
-  //   }
-
-  //   //when the guess is too low
-  // } else if (guess < secretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector('.message').textContent = 'Too low';
-  //     score--;
-  //     document.querySelector('.score').textContent = score;
-  //   } else {
-  //     document.querySelector('.message').textContent = 'You lose the game';
-  //     document.querySelector('.score').textContent = 0;
-  //   }
-  // }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
